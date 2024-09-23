96f36cee3d10cc6a74d0b5dc8fed1aefa9df1386
hananhamdy <hananhamdy92@gmail.com>
2024-09-23 18:03:51 +0300

    core components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardBookComponent } from './shared/card-book/card-book.component';
+import { CardEmptyComponent } from './shared/card-empty/card-empty.component';
+import { CardLoadingComponent } from './shared/card-loading/card-loading.component';
+import { CardFailureComponent } from './shared/card-failure/card-failure.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardBookComponent,
+    CardEmptyComponent,
+    CardLoadingComponent,
+    CardFailureComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
